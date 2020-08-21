337a0d0e61d661cf5c91d7495a42adf154c84191
ituageorge <50578363+ituageorge@users.noreply.github.com>
2020-08-21 23:43:18 +0100

    transfer of money to other peoples bank account

--- src/js/components/MyAccountTransfer.js
+++ src/js/components/MyAccountTransfer.js
@@ -1,36 +1,36 @@
-// import React from 'react';
+import React from 'react';
 
-// const MyAccountTransfers = () => {
+const MyAccountTransfers = () => {
 
-//   return(
-// <div className="">
-// <form>
+  return(
+<div className="">
+<form>
 
-// <div className="">
-//          <select ref="selectValue" onChange={this.handleChange}>
-//             <option value="555">United Bank for Africa</option>
-//             <option value="777">Union Bank</option>
-//             <option value="444">First Bank </option>
-//             <option value="333">Access Bank</option>
+<div className="">
+         <select ref="selectValue" onChange={this.handleChange}>
+            <option value="555">United Bank for Africa</option>
+            <option value="777">Union Bank</option>
+            <option value="444">First Bank </option>
+            <option value="333">Access Bank</option>
 
-//             <option value="222">Zenith Bank</option>
-//             <option value="111">First City Monument Bank of Nigeria</option>
-//             <option value="888">Fidelity Bank of Nigeria </option>
-//             <option value="999">Guaranty Trust Bank</option>
-//           </select>
-//     </div>
+            <option value="222">Zenith Bank</option>
+            <option value="111">First City Monument Bank of Nigeria</option>
+            <option value="888">Fidelity Bank of Nigeria </option>
+            <option value="999">Guaranty Trust Bank</option>
+          </select>
+    </div>
 
-//   <div className="form-group">
-//     <label htmlFor="formGroupExampleInput2">Input Amount</label>
-//     <input type="number" className="form-control" id="my-amt" placeholder="Amount"/>
-//   </div>
-//   <button>Recharge</button>
-// </form>
+  <div className="form-group">
+    <label htmlFor="formGroupExampleInput2">Input Amount</label>
+    <input type="number" className="form-control" id="my-amt" placeholder="Amount"/>
+  </div>
+  <button>Recharge</button>
+</form>
 
-// <div className="">
-//   {/* <p>`*${bankKey}*${amount}#`</p> */}
-// </div>
-// </div>
-//   );
-// };
-// export default MyAccountTransfers;
+<div className="">
+  {/* <p>`*${bankKey}*${amount}#`</p> */}
+</div>
+</div>
+  );
+};
+export default MyAccountTransfers;
--- src/js/components/OthersTransfer.js
+++ src/js/components/OthersTransfer.js
@@ -1,33 +1,61 @@
 import React from 'react';
 
-const useStateWithLocalStorage = bankLocalStorageKey => {
-  const [bankSelected, setBankSelected] = React.useState(
-    localStorage.getItem(bankLocalStorageKey) || ''
+const useStateWithLocalStorage = (localStorageKey) => {
+  const [amountInputed, setAmountInputed] = React.useState(
+    localStorage.getItem(localStorageKey) || ''
+  );
+  const [phoneNumInputed, setPhoneNumInputed] = React.useState(
+    localStorage.getItem(localStorageKey) || ''
+  );
+  const [selectedBank, setSelectedBank] = React.useState(
+    localStorage.getItem(localStorageKey) || ''
   );
 
   React.useEffect(() => {
-    localStorage.setItem(bankLocalStorageKey, bankSelected);
-  }, [bankSelected]);
+    localStorage.setItem(localStorageKey, phoneNumInputed);
+  }, [phoneNumInputed]);
 
-  return [bankSelected, setBankSelected];
+  React.useEffect(() => {
+    localStorage.setItem(localStorageKey, amountInputed);
+  }, [amountInputed]);
+
+  React.useEffect(() => {
+    localStorage.setItem(localStorageKey, selectedBank);
+  }, [selectedBank]);
+
+
+  return [amountInputed, setAmountInputed, phoneNumInputed, setPhoneNumInputed, selectedBank, setSelectedBank];
 }
 
 const OthersTransfer = () => {
-  const [bankSelected, setBankSelected] = useStateWithLocalStorage(
-    'myValueInLocalStorage'
+  const [amountInputed, setAmountInputed] = useStateWithLocalStorage(
+    'amountInputedLocalStorage'
   );
 
-  const onChange = event => setBankSelected(event.target.value);
+  const [phoneNumInputed, setPhoneNumInputed] = useStateWithLocalStorage(
+    'phoneNumInputedLocalStorage'
+  );
+
+  const [selectedBank, setSelectedBank] = useStateWithLocalStorage(
+    'bankSelectedLocalStorage'
+  );
+
+  const onChange = event => setAmountInputed(event.target.value);
+  const phoneChange = event => setPhoneNumInputed(event.target.value)
+  const bankChange = event => setSelectedBank(event.target.value)
+
  const mySubmitHandler = (event) => {
     event.preventDefault();
-    alert("You are submitting " + bankSelected);
+    alert("You are submitting " + `${amountInputed}x${phoneNumInputed}x${selectedBank}#`);
   }
 
   return(
   <div className="">
     <form onSubmit={mySubmitHandler}>
-    {/* <div className="">
-         <select ref="selectValue" onChange={onChange}>
+    <div className="">
+         <select 
+        //  ref="selectValue" 
+         onClick={bankChange}>
             <option value="555">United Bank for Africa</option>
             <option value="777">Union Bank</option>
             <option value="444">First Bank </option>
@@ -38,17 +66,18 @@ const OthersTransfer = () => {
             <option value="888">Fidelity Bank of Nigeria </option>
             <option value="999">Guaranty Trust Bank</option>
           </select>
-    </div> */}
+    </div>
 
   <div className="form-group">
-    <label htmlFor="formGroupExampleInput">Input Amount</label>
-    <input value={bankSelected} type="number" className="form-control" id="bank" placeholder="Amount" onChange={onChange} />
+    <label htmlFor="formGroupExampleInput">Input amount to send</label>
+    <input value={amountInputed} type="number" className="form-control" id="bank" placeholder="Amount" onChange={onChange} />
   </div>
 
   <div className="form-group">
     <label htmlFor="formGroupExampleInput2">Input Receivers phone number</label>
-    {/* <input value={} type="number" className="form-control" id="phone" placeholder="phone number"/> */}
+    <input value={phoneNumInputed} type="number" className="form-control" id="phone" placeholder="phone number" onChange={phoneChange}/>
   </div>
+
   <div className="">
   <button type="submit" className="btn btn-primary">Recharge</button>
   </div>
@@ -56,10 +85,10 @@ const OthersTransfer = () => {
 </form>
 <div className="">
 {/* <p>`*${bankKey}*${phoneNum}*${amount}#`</p> */}
-<p>`*${bankSelected}*$#`</p>
+  <p>`*${phoneNumInputed}${amountInputed}*${selectedBank}#`</p>
 </div>
 </div>
   )
-}
+};
 
 export default OthersTransfer;
